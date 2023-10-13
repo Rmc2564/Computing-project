9544a3d7cd4b25af66f06bfe73b75685ec12e718
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2023-10-13 12:37:31 +0100

    Create Bisection.py

---
+++ Bisection.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 15:20:59 2023
+
+@author: alexa
+"""
+
+
+
+def bisection(f,r0,r1,tolerance): #applies the bisection method to a function f with initial guesses rlow and rhigh
+    error = 'No sign change in given interval'
+    if f(r0)*f(r1) > 0:
+        return error
+
+    if f(r0) > 0:
+        ra = r0
+        rb = r1
+    else:
+        ra = r1
+        rb = r0
+    while abs(ra - rb) > tolerance:
+        m = (ra+rb)/2
+        if f(m) > 0:
+            ra = m
+        else:
+            rb = m
+    return m
+
