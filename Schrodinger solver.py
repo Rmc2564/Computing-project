dbd5cc3e2d12f15e912742e6bc150b03c02a0b33
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2023-10-17 15:36:56 +0100

    Update Schrodinger solver.py

--- Schrodinger solver.py
+++ Schrodinger solver.py
@@ -5,25 +5,6 @@ Created on Thu Oct 12 15:20:59 2023
 @author: alexa
 """
 
-def bisection(f,r0,r1,tolerance): #applies the bisection method to a function f with initial guesses rlow and rhigh
-    error = 'No sign change in given interval'
-    if f(r0)*f(r1) > 0:
-        return error
-
-    if f(r0) > 0:
-        ra = r0
-        rb = r1
-    else:
-        ra = r1
-        rb = r0
-    while abs(ra - rb) > tolerance:
-        m = (ra+rb)/2
-        if f(m) > 0:
-            ra = m
-        else:
-            rb = m
-    return m
-
 import numpy as np
 from scipy.integrate import odeint, simpson
 import matplotlib.pyplot as plt
@@ -46,9 +27,9 @@ mu = 0.5110
 y0 = [0,1]
 a0 = 1/(mu*alpha)
 
-solution = odeint(schrodinger, y0, rs, (E, l, mu)) #Gives un-normalised wavefunction
+solution = odeint(schrodinger, y0, rs, (E, l, mu), rtol = 1e-20) #Gives un-normalised wavefunction
 wvfunc = solution[:,0]/rs
-dist = wvfunc*wvfunc
+dist = rs**2*wvfunc*wvfunc
 'now just need to normalise'
 
 norm = simpson(dist, dx = 0.1)
@@ -59,7 +40,7 @@ E = E/4
 l = 0
 sol2 = odeint(schrodinger, y0, rs, (E,l,mu))
 wvfnc2 = sol2[:,0]/rs
-dist2 = wvfnc2*wvfnc2
+dist2 = rs**2*wvfnc2*wvfnc2
 
 norm2 = simpson(dist2, dx = 0.1)
 pdist2 = dist2/norm2
