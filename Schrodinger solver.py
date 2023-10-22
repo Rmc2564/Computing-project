a5c8e139ce789f3f0476db83c7b200a5ce428045
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2023-10-22 20:33:05 +0100

    Update Schrodinger solver.py

--- Schrodinger solver.py
+++ Schrodinger solver.py
@@ -6,7 +6,7 @@ Created on Thu Oct 12 15:20:59 2023
 """
 
 import numpy as np
-from scipy.integrate import odeint, simpson, solve_ivp
+from scipy.integrate import odeint, simpson
 import matplotlib.pyplot as plt
 
 alpha = 1/137
@@ -20,8 +20,8 @@ def schrodinger(y,r, E, l, mu):
 
 #Natural units in MeV
 
-rs = np.linspace(4000,1,100000)
-E = -1.360569*10**-5
+rs = np.linspace(3500,1,100000)
+E = -1.304569*10**-5
 l = 0
 mu = 0.5110
 y0 = [0,1]
@@ -30,25 +30,85 @@ a0 = 1/(mu*alpha)
 
 
 solution = odeint(schrodinger, y0, rs, (E, l, mu), atol = 1e-20) #Gives un-normalised wavefunction
-u = solution[:,0][::-1]
-rs = rs[::-1]
+u = solution[:,0]
+plt.plot(rs/a0,u)
+plt.show()
 norm = simpson(u*u, x = rs)
 unorm = u/np.sqrt(abs(norm))
 
 pdist = unorm*unorm
+print(unorm[-1])
+#plt.plot(rs/a0,pdist)
+#plt.title('ground state')
+'Now have a method for solving the schrodinger equation for a given energy,'
+'just need to implement a modified bisection method'#
 
-plt.plot(rs/a0,pdist)
 
-E = E/4
-l = 0
+def getEnergy(E0, E1, tolerance, l):
+    E0 = E0*10**-6
+    E1 = E1*10**-6
 
-solution_2_0 = odeint(schrodinger, y0, rs, (E, l, mu))
-u20 = solution_2_0[:,0][::-1]
+    
+    t1 = odeint(schrodinger, y0, rs, (E1, l, mu))[:,0]
+    t0 = odeint(schrodinger, y0, rs, (E0, l, mu))[:,0]
+    norm1 = simpson(t1, x = rs)
+    norm0 = simpson(t0, x = rs)
+    t0 = t0/np.sqrt(norm0)
+    t1 = t1/np.sqrt(norm1)
+    test_0 = t0[-1]
+    test_1 = t1[1]
+    error = 'No sign change on interval'
+    
+    if test_0*test_1 >0:
+        return error
+    
+    if test_0 < 0:
+        Elow = E0
+        Ehigh = E1
+    else:
+        Ehigh  = E0
+        Elow = E1
+    Emid = (Ehigh+Elow)*0.5
+    print(abs(test_0 - test_1))
+    while abs(test_0 - test_1) > tolerance:
+        
+        
+        tlow = odeint(schrodinger, y0, rs, (Elow, l, mu))[:,0]
+        normlow = simpson(tlow*tlow, x = rs[::-1])
+        tlow = tlow/np.sqrt(normlow)
+        test_0 = tlow[-1]
+
+        thigh = odeint(schrodinger, y0, rs, (Ehigh, l, mu))[:,0]
+        normhigh = simpson(thigh*thigh, x = rs[::-1])
+        print(Emid*10**6)
+        thigh = thigh/np.sqrt(normhigh)
+        test_1 = thigh[-1]
+        
+        Emid = (Ehigh + Elow)*0.5
+       
+        tmid = odeint(schrodinger, y0, rs, (Emid, l, mu))[:,0]
+        normmid = simpson(tmid*tmid, x = rs[::-1])
+        tmid = tmid/np.sqrt(normmid)
+        
+        testmid = tmid[-1]
+        plt.plot(rs/a0, tmid)
+        if testmid < 0:
+            Elow = Emid
+        
+        else:
+            Ehigh = Emid
+
+    return Emid
+
+    
+
+Eground = getEnergy(-13.4,-13.8,0.00001,0)
+solution = odeint(schrodinger, y0, rs, (Eground, l, mu), atol = 1e-20) #Gives un-normalised wavefunction
+u = solution[:,0][::-1]
 rs = rs[::-1]
-norm20 = simpson(u20*u20, x = rs)
-u20norm = u20/np.sqrt(abs(norm20))
 
-pdist20 = u20norm*u20norm
+#norm = simpson(u*u, x = rs)
+#unorm = u/np.sqrt(abs(norm))
 
-plt.plot(rs/a0, pdist20)
-
+#pdist = unorm*unorm
+#plt.plot(rs/a0,pdist)
