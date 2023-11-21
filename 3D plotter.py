e9383d4b9653402f79d7baceb9f21c05163c723e
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2023-11-21 16:55:34 +0000

    Plot Mutiple Spherical Harmonics
    Currently Broken

--- 3D plotter.py
+++ 3D plotter.py
@@ -9,41 +9,44 @@ from scipy.special import sph_harm
 import numpy as np
 from matplotlib import cm, colors
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+#from mpl_toolkits import mplot3d
 
 
 phi = np.linspace(0, np.pi, 150)
 theta = np.linspace(0, 2*np.pi, 150)
 phi, theta = np.meshgrid(phi, theta)
 
+Energies = [-13.612844276428223, -3.37939453125, -3.392089843749999]
+ls = [0, 0, 1]
 
-
-m, l = 1, 1
-
-# Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
-fcolors = np.abs(sph_harm(m, l, theta, phi))
-#fcolors_bar = np.conjugate(fcolors)
-#fcolors = np.sqrt(np.real(fcolors*fcolors_bar))
-fmax, fmin = fcolors.max(), fcolors.min()
-
-
-X = fcolors*np.sin(phi) * np.cos(theta)
-Y = fcolors*np.sin(phi) * np.sin(theta)
-Z = fcolors*np.cos(phi)
-
-fcolors = (fcolors - fmin)/(fmax - fmin)
-
-# fig = plt.figure()
-# ax = plt.axes(projection = '3d')
-# ax.view_init(20,30)
-# ax.plot_surface(x, y, z,  rstride=1, cstride=1, facecolors=cm.viridis(fcolors))
+l = 1
+ms = [-1,0,1]
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+pos =1
 
-ax.plot_surface(X, Y, Z, rstride=5, cstride=5, facecolors=cm.viridis(fcolors))
+Coords = []
+for m in ms:
+    fcolors = np.abs(sph_harm(m, l, theta, phi))
+    
+    fmax, fmin = fcolors.max(), fcolors.min()
+    
+    
+    X = fcolors*np.sin(phi) * np.cos(theta)
+    Y = fcolors*np.sin(phi) * np.sin(theta)
+    Z = fcolors*np.cos(phi)
+    
+    Coords = Coords + [(X,Y,Z)]
+    
+   # fcolors = (fcolors - fmin)/(fmax - fmin)
 
-# rotate the axes and update
-
+    # ax = fig.add_subplot( projection='3d')
+    # pos = pos+1
+    # ax.plot_surface(X, Y, Z, rstride=5, cstride=5, facecolors=cm.viridis(fcolors))
+    
+    # rotate the axes and update
+    
+for i in range(0,3):
+    
 for angle in range(0, 360):
     ax.view_init(20, angle)
     plt.draw()
