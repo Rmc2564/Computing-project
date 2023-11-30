bc00959fd33463f0e0cd885c1b6c36e55b251b7b
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2023-11-30 12:57:24 +0000

    Update 3D plotter.py
    Got the spherical harmonic plots and improved commenting for readability

--- 3D plotter.py
+++ 3D plotter.py
@@ -9,45 +9,31 @@ from scipy.special import sph_harm
 import numpy as np
 from matplotlib import cm, colors
 import matplotlib.pyplot as plt
-#from mpl_toolkits import mplot3d
-
+from mpl_toolkits import mplot3d
+from scipy.integrate import odeint, simpson
 
 phi = np.linspace(0, np.pi, 150)
 theta = np.linspace(0, 2*np.pi, 150)
 phi, theta = np.meshgrid(phi, theta)
 
-Energies = [-13.612844276428223, -3.37939453125, -3.392089843749999]
-ls = [0, 0, 1]
+'The angular dependance is given by the spherical haronics, which are visualised'
+'using this code'
+
 
 l = 1
-ms = [-1,0,1]
+m = 1
+
+sph = np.abs(sph_harm(m, l, theta, phi))
+    
+smax, smin = sph.max(), sph.min()
+col = (sph - smin)/(smax - smin)
+
+X = sph*np.sin(phi) * np.cos(theta)
+Y = sph*np.sin(phi) * np.sin(theta)
+Z = sph*np.cos(phi)
+
 fig = plt.figure()
-pos =1
 
-Coords = []
-for m in ms:
-    fcolors = np.abs(sph_harm(m, l, theta, phi))
-    
-    fmax, fmin = fcolors.max(), fcolors.min()
-    
-    
-    X = fcolors*np.sin(phi) * np.cos(theta)
-    Y = fcolors*np.sin(phi) * np.sin(theta)
-    Z = fcolors*np.cos(phi)
-    
-    Coords = Coords + [(X,Y,Z)]
-    
-   # fcolors = (fcolors - fmin)/(fmax - fmin)
-
-    # ax = fig.add_subplot( projection='3d')
-    # pos = pos+1
-    # ax.plot_surface(X, Y, Z, rstride=5, cstride=5, facecolors=cm.viridis(fcolors))
-    
-    # rotate the axes and update
-    
-for i in range(0,3):
-    
-for angle in range(0, 360):
-    ax.view_init(20, angle)
-    plt.draw()
-    plt.pause(.01)
+ax = fig.add_subplot( projection='3d')
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, facecolors=cm.viridis(col))
+ax.set_title('m = 1', fontsize = '15')
