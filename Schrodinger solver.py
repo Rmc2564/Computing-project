89f63c1454d436a7ccaa6c30f6e2f9c2d210d14c
Rmc2564 <147627938+Rmc2564@users.noreply.github.com>
2024-03-24 08:35:42 +0000

    Big commit now I have my laptop back
    Laptop broken so big update:
    Fixed beta definition so the code actually runs now
    Added standard deviation check to ensure numerical precision in the energies being calculated
    Wrote graphing code for the energy level diagram

--- Schrodinger solver.py
+++ Schrodinger solver.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 12 15:20:59 2023
-
-@author: alexa
-"""
-
 import numpy as np
 from scipy.integrate import odeint, simpson
 import matplotlib.pyplot as plt
@@ -23,7 +16,7 @@ def schrodinger(y,r, potential, E, l, mu):
 
 #Natural units in MeV
 
-rs = np.linspace(0.0000000000001,14, 1500)
+rs = np.linspace(0.00000001,14, 1500)
 E = -1.304569*10**-5
 l = 0
 mu = 0.51099895000
@@ -65,8 +58,8 @@ def getEnergy(E0, E1, potential, tolerance, l, mu):
     test_0 = t0[-1]
     test_1 = t1[-1]
     error = 'No sign change on interval'
-    print('test_0: ' + str(test_0))
-    print('test_1 : ' + str(test_1))
+    #print('test_0: ' + str(test_0))
+    #print('test_1 : ' + str(test_1))
     if test_0*test_1 >0:
         return error
     
@@ -79,9 +72,10 @@ def getEnergy(E0, E1, potential, tolerance, l, mu):
     Emid = (Ehigh+Elow)*0.5
     #overflow_check = np.max(t1)
     #print(abs(test_0 - test_1))
-    while abs(test_0 - test_1) > tolerance:
+    count = 0
+    while abs(Elow - Ehigh) > tolerance or count<60:
         
-        
+        count = count+1
         tlow = odeint(schrodinger, y0, rs, (potential, Elow, l, mu))[:,0]
         normlow = np.abs(simpson(tlow*tlow, x = rs[::-1]))
         tlow = tlow/np.sqrt(normlow)
@@ -89,7 +83,7 @@ def getEnergy(E0, E1, potential, tolerance, l, mu):
 
         thigh = odeint(schrodinger, y0, rs, (potential, Ehigh, l, mu))[:,0]
         normhigh = np.abs(simpson(thigh*thigh, x = rs[::-1]))
-        print(Emid)
+        #print(Emid)
         thigh = thigh/np.sqrt(normhigh)
         test_1 = thigh[-1]
         
@@ -99,8 +93,8 @@ def getEnergy(E0, E1, potential, tolerance, l, mu):
         tmid = tmid/np.sqrt(normmid)
         
         testmid = tmid[-1]
-        # plt.plot(rs/a0, tmid)
-        # plt.title('Get smeared, idiot')
+        #plt.plot(rs/a0, tmid)
+        #plt.title('Get smeared, idiot')
         
         if testmid < 0:
             Elow = Emid
@@ -124,19 +118,19 @@ def milestone(ns, potential, energy_start, ls, mu):
         E_init = energy_start[i]
         E0 = E_init + 0.1
         E1 = E_init - 0.1
-        print(E0, E1)
+        #print(E0, E1)
         l = ls[i]
         i = i+1
         
-        Enew = getEnergy(E0, E1, potential, 0.01, l, mu)
-        print('Enew = ' + str(Enew))
+        Enew = getEnergy(E0, E1, potential, 0.0001, l, mu)
+        #print('Enew = ' + str(Enew))
         Es.append(Enew)
         #error = (E_anal- Enew)/E_anal
         #print('Energy error: ' + str(error) + '%')
         # u = odeint(schrodinger, y0, rs, (coulomb, E, l, mu))[:,0]
         # unorm = np.abs(simpson(u,x = rs))
         # u = u/np.sqrt(unorm)
-            
+        
     return Es
             
 ns = [1,2]
@@ -260,17 +254,11 @@ B0 = 0.185
 B1 = 0.205
 
 B_lit_bottom = 0.168
-beta_charm = get_beta(B0,B1,alpha_s_charm, 0.001, 0, mu_charm, E0_charm)
-print(beta_charm)
-
-B0 = 0.158
-B1 = 0.178
-E0_bottom = 0.02
-beta_bottom = get_beta(B0,B1,alpha_s_bottom, 0.001, 0, mu_bottom, E0_bottom)
-
+beta = get_beta(B0,B1,alpha_s_charm, 0.001, 0, mu_charm, E0_charm)
+print('beta: ' + str(beta))
 
 def cornell_charm(r):
-    return (-4*alpha_s_charm)/(3*r) + beta_charm*r 
+    return (-4*alpha_s_charm)/(3*r) + beta*r 
 
 def cornell_bottom(r):
     return (-4*alpha_s_bottom)/(3*r) + beta*r
@@ -284,46 +272,55 @@ def cornell_bottom(r):
 
 #spin_coupling = ((8*alpha_s_charm)/(9*1.34**2))*np.abs(R_zero)**2
 
-accessible_cols = ['#FFB000','#DC267F','#648FFF']
+V0_charm = np.sqrt((4*alpha_s_charm)/(3*beta))
+
+accessible_cols = ['red','blue','black']
 Energies = milestone([1,1,2], cornell_charm, [E0_charm,0.9, 1.1], [0,1,0], mu_charm)  
 Energies_bottom = milestone([1,1,2], cornell_bottom, [0.01, 0.4,0.6], [0,1,0], mu_bottom)   
 ls = [0,1,0]
 ns = [1,1,2]
 
+print('charm energies: ' + str(Energies))
+print('Bottom energies: ' + str(Energies_bottom))
+
 plt.figure(2, figsize = (11,7), frameon=False)
+plt.axhline(0,0,14, linestyle = 'dashdot', color = 'grey')
 for i in range(0,3):
     n = ns[i]
     l = ls[i]
-    u = odeint(schrodinger, y0, rs, (cornell_bottom, Energies_bottom[i], l, mu_bottom))[:,0]
+    u = odeint(schrodinger, y0, rs, (cornell_charm, Energies[i], l, mu_charm))[:,0]
     unorm = np.abs(simpson(u*u,x = rs[::-1]))
     u = u/np.sqrt(unorm)
     prob = u*u
     plt.plot(rs, u,label = '(n,l) = ' + str((n,l)), color = accessible_cols[i])    
 plt.legend(fontsize = '17')
 
-plt.xlim(0,13)
+plt.xlim(0,14)
 plt.xlabel('Quark seperation $(GeV^{-1})$', fontsize = '22')
 plt.xticks(fontsize = '20')
 
-plt.ylabel('radial probability density', fontsize = '22')
+plt.ylabel('u(r) $(Gev^{1/2})$', fontsize = '22')
 #plt.ylim(0,0.45)
 plt.yticks(fontsize = '17')
+#for pos in ['right', 'top']: 
+    #plt.gca().spines[pos].set_visible(False) 
+#plt.show() 
 
-for pos in ['right', 'top']: 
-    plt.gca().spines[pos].set_visible(False) 
-plt.show() 
+#plt.vlines(V0_charm, 0,0.68,)
+plt.savefig('Charmonium wavefunctions.pdf',bbox_inches="tight")
+
 
 '''if given odeint solution, returns the derivative at 0'''
 def getderiv_0(solution): 
-    print("make sure you have input a raw odeint solution or this wont work")
+    #print("make sure you have input a raw odeint solution or this wont work")
     u = solution[:,0]
     unorm = np.abs(simpson(u*u,x = rs[::-1]))
     solution = solution/np.sqrt(unorm)
     deriv = solution[:,1]
     return deriv[0]
 
-E_big = E0_charm + 0.2
-E_small = E0_charm - 0.2
+E_big = E0_charm + 0.1
+E_small = E0_charm - 0.1
 
 u0_big = odeint(schrodinger, y0, rs, (cornell_charm, E_big, 0, mu_charm))[:,0]
 u0_big = u0_big/np.sqrt(np.abs(simpson(u0_big*u0_big, x = rs)))
@@ -337,19 +334,20 @@ plt.plot(rs,u0_big, color = 'blue', label = 'Energy = 0.588 GeV')
 plt.plot(rs,u0_actual, color = 'black', label = 'Energy = 0.388 GeV')
 plt.plot(rs,u0_small, color = 'r', label = 'Energy = 0.188 GeV')
 plt.xlim(0,14)
-plt.legend(loc = 'upper left', fontsize = '13')
-plt.xlabel('Quark seperation $(GeV^{-1})$', fontsize = '13')
-plt.ylabel('u(r) $(Gev^{3/2})$',fontsize = '13')
-plt.xticks(fontsize = '15')
-plt.yticks(fontsize = '15')
-
-for pos in ['right', 'top']: 
-    plt.gca().spines[pos].set_visible(False) 
+plt.legend(loc = 'upper left', fontsize = '14')
+plt.xlabel('Quark seperation $(GeV^{-1})$', fontsize = '22')
+plt.ylabel('u(r) $(Gev^{1/2})$',fontsize = '22')
+plt.xticks(fontsize = '16')
+plt.yticks(fontsize = '16')
+plt.savefig('Bisection illustration.pdf',bbox_inches="tight")
+#for pos in ['right', 'top']: 
+#    plt.gca().spines[pos].set_visible(False) 
 plt.show()
+
 
 def spin_split(u,mass,alpha):
     R0 = getderiv_0(u)
-    delta = ((32*np.pi*alpha)/(9*mass*mass))*np.abs(R0*R0)
+    delta = ((8*alpha)/(9*mass*mass))*R0**2
     return delta
 
 '''deltas give prefactor to spin dot product'''
@@ -363,8 +361,8 @@ bottom_antialigned = []
 for i in range(0,3):
     Ec = Energies[i]
     Eb = Energies_bottom[i]
-    uc = odeint(schrodinger, y0, rs, (cornell_charm, Ec, 0, mu_charm))
-    ub = odeint(schrodinger, y0, rs, (cornell_bottom, Eb, 0, mu_bottom))
+    uc = odeint(schrodinger, y0, rs, (cornell_charm, Ec, [0,1,0][i], mu_charm))
+    ub = odeint(schrodinger, y0, rs, (cornell_bottom, Eb, [0,1,0][i], mu_bottom))
     split_c = spin_split(uc,2*mu_charm,alpha_s_charm)
     split_b = spin_split(ub,2*mu_bottom,alpha_s_bottom)
     charm_aligned.append(split_c*0.25)
@@ -374,5 +372,52 @@ for i in range(0,3):
     
 print('charm aligned: ' + str(charm_aligned))
 print('charm anti aligned:' + str(charm_antialigned))
-print('charm aligned: ' + str(bottom_aligned))
-print('charm antialigned: ' + str(bottom_aligned))
+print('bottom aligned: ' + str(bottom_aligned))
+print('bottom antialigned: ' + str(bottom_antialigned))
+
+
+'''Extra code for finding the LS splitting'''
+corrections_charm = []
+corrections_bottom = []
+
+LS = [0,-2,-1,1]
+DV_charm = (2*alpha_s_charm)/(2*1.34**2*rs**3) - beta/(2*1.34**2*rs)
+DV_bottom = (2*alpha_s_bottom)/(2*1.34**2*rs**3) - beta/(2*1.34**2*rs)
+U_state_charm = odeint(schrodinger, y0, rs, (cornell_charm, Energies[1], l, mu_charm))[:,0]
+unorm = np.abs(simpson(U_state_charm**2,x = rs[::-1]))
+U_state_charm = U_state_charm/np.sqrt(unorm)
+U_state_bottom = odeint(schrodinger, y0, rs, (cornell_charm, Energies_bottom[1], l, mu_bottom))[:,0]
+unorm = np.abs(simpson(U_state_bottom**2,x = rs[::-1]))
+U_state_bottom = U_state_bottom/np.sqrt(unorm)
+
+for i in LS:
+    LS_state = LS[i]
+    DV_c = DV_charm*LS_state
+    Correction_charm  = simpson(U_state_charm**2*DV_c, x = rs)
+    corrections_charm.append(Correction_charm)
+    
+    DV_b = DV_bottom*LS_state
+    Correction_bottom = simpson(U_state_bottom**2*DV_c, x = rs)
+    corrections_bottom.append(Correction_bottom)
+print("Charm P corrections: " + str(corrections_charm))
+print("Bottom P corrections: " + str(corrections_bottom))
+
+
+energies_check = []
+
+for i in range(0,100):
+    E_it = milestone([1,1,2], cornell_charm, [E0_charm,0.9, 1.1], [0,1,0], mu_charm)  
+    energies_check.append(E_it)
+    
+energies_check = np.array(energies_check)
+ground = energies_check[:,0]
+first = energies_check[:,1]
+second = energies_check[:,2]
+xs = [1]*len(ground)
+checkg = np.std(ground)
+checkf = np.std(first)
+checks = np.std(second)
+
+print('Ground state stdev '+str(checkg))
+print('(1,1) state stdev ' +str(checkf))
+print('(2,0) state stdev ' +str(checks))
